b3f42d41cc1de5486d20c8b2f3f4ab9f6c6e3496
WIN_NT\x16g027 <x16g027@chiba-fjb.ac.jp>
2018-10-26 11:53:28 +0900

    オリジナル

---
+++ app/src/main/java/jp/ac/chiba_fjb/x16g027/x16g_a/MainActivity.java
@@ -0,0 +1,13 @@
+package jp.ac.chiba_fjb.x16g027.x16g_a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
