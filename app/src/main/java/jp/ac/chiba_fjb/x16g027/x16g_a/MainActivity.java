36d50c87249f33bc706726371c447d6958bc9fab
WIN_NT\x16g027 <x16g027@chiba-fjb.ac.jp>
2018-11-07 11:36:17 +0900

    メニュー追加

--- app/src/main/java/jp/ac/chiba_fjb/x16g027/x16g_a/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/x16g027/x16g_a/MainActivity.java
@@ -1,7 +1,10 @@
 package jp.ac.chiba_fjb.x16g027.x16g_a;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // メニューをActivity上に設置する
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // 参照するリソースは上でリソースファイルに付けた名前と同じもの
+        getMenuInflater().inflate(R.menu.option, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // メニューが選択されたときの処理
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuItem1:
+                Intent intent1 = new Intent(MainActivity.this, Constellation.class);
+                startActivity(intent1);
+                return true;
+
+            case R.id.menuItem2:
+                Intent intent2 = new Intent(MainActivity.this, Moon.class);
+                startActivity(intent2);
+                return true;
+
+            case R.id.menuItem3:
+                Intent intent3 = new Intent(MainActivity.this, Weather.class);
+                startActivity(intent3);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
